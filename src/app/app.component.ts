3c279883582192f361b0cdc6d29cf5bd6456c0c3
Jose William Martinez Pena <wilhouse180318@gmail.com>
2023-07-07 12:12:36 -0500

    Se realiza otro ejemplo de angular.cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Link } from './link/link.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'newTry-Angular';
+  links: Link[];
+
+  constructor(){
+    this.links = [
+      new Link ('angular', 'http://angular.io', 100),
+      new Link ('Google', 'http://google.com', 100),
+      new Link ('Youtube', 'http://youtube.com', 100),
+    ]
+    console.log(this.links)
+  }
+  addLink(title: HTMLInputElement, link: HTMLInputElement){
+    this.links.push(new Link(title.value , link.value))
+    title.value = "";
+    link.value = " ";
+    return false;
+  }
 }
